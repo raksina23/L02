ec6de078c452d01c445b94f82aa83de870a5f75d
raksina23 <raksina290125@gmail.com>
2025-09-23 14:17:18 +0700

    committingf rest of the file

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+
+import redis
+from fastapi import FastAPI
+
+app = FastAPI()
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.decr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.get("/")
+async def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+    
